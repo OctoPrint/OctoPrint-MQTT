c46cfb41be2c6c7563244287277cd6f995323695
Gina Häußge <gina@octoprint.org>
2017-07-06 15:00:22 +0200

    Some visual refactoring on settings page

--- octoprint_mqtt/static/js/mqtt.js
+++ octoprint_mqtt/static/js/mqtt.js
@@ -4,11 +4,21 @@ $(function() {
         var self = this;
 
         self.global_settings = parameters[0];
-        
+
+        self.showUserCredentials = ko.observable(false);
+        self.showSsl = ko.observable(false);
+
+        self.settings = undefined;
         self.availableProtocols = ko.observableArray(['MQTTv31','MQTTv311']);
       
         self.onBeforeBinding = function () {
             self.settings = self.global_settings.settings.plugins.mqtt;
+
+            // show credential options if username is set
+            self.showUserCredentials(!!self.settings.broker.username());
+
+            // show SSL/TLS config options if any of the corresponding settings are set
+            self.showSsl(!!self.settings.broker.tls && !!self.settings.broker.tls.cacerts && !!self.settings.broker.tls.cacerts())
         };
     }
 
